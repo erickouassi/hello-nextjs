51ce2a46ccdb6608eebab0174b85324bf6bf8b26
Glitch (ek-hello-nextjs) <none>
2019-08-20 11:52:30 +0000

    📉📮 Updated with Glitch

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+// import Header from '../pages/components/Header';
+import Layout from '../components/MyLayout';
+
+export default function About() {
+    return (
+        <div>
+            <Layout>
+                <p>Hello about Next.js</p>
+                </Layout>
+        </div>
+    );
+}
